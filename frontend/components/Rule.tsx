4b227168b754b51f8406d529c9ebd5816aef4f7d
Luukuton <mleinikka@gmail.com>
2021-07-09 16:18:26 +0300

    Add support for hash rule links

--- frontend/components/Rule.tsx
+++ frontend/components/Rule.tsx
@@ -1,10 +1,10 @@
 import * as types from '../types'
 
-export default function Rule({ data }: { data: types.Rule }): JSX.Element {
-  return <div style={{whiteSpace: "pre-wrap"}}>
+export default function Rule({ data, subId }: { data: types.Rule, subId: number }): JSX.Element {
+  return <div id={`${subId}.${data.id}`} style={{whiteSpace: "pre-wrap"}}>
     {data.id}{': '} {data.content}
     <ol type='a'>
-      {Object.entries(data.subrules).map(([key, value]) => <li key={key}>{value}</li>) }
+      {Object.entries(data.subrules).map(([key, value]) => <li key={key} id={`${subId}.${data.id}${key}`}>{value}</li>) }
     </ol>
     <br/>
  </div>
--- frontend/components/Subchapter.tsx
+++ frontend/components/Subchapter.tsx
@@ -2,8 +2,8 @@ import * as types from '../types'
 import Rule from "./Rule"
 
 export default function Subchapter({ data }: { data: types.Subchapter }): JSX.Element {
-  return <div>
+  return <div id={`${data.id}`}>
     <h4>{data.id}. {data.title}</h4>
-    <div>{data.rules.map((rule: types.Rule) => <Rule key={rule.id} data={rule} />) }</div>
+    <div>{data.rules.map((rule: types.Rule) => <Rule key={rule.id} subId={data.id} data={rule} />) }</div>
   </div>
 }
