6385edec9814f5d82965237bf6461372c79bed05
Luukuton <mleinikka@gmail.com>
2021-07-10 23:25:39 +0300

    Add documentation for lib

--- frontend/lib/api.tsx
+++ frontend/lib/api.tsx
@@ -1,9 +1,22 @@
 import * as types from "../types"
 
+/**
+ * Gets the API URL for rules.
+ *
+ * @param path
+ * @returns API URL as string
+ */
 export function getAPIURL(path = ""): string {
   return `${process.env.RULEBOOK_API_URL}/chapters${path}`
 }
 
+/**
+ * Fetches JSON response from the API.
+ *
+ * @param path
+ * @param query
+ * @returns JSON response as Rulebook
+ */
 export async function fetchAPI(path: string, query = ""): Promise<types.Rulebook> {
   const requestUrl = getAPIURL(path) + query
   const response = await fetch(requestUrl)
--- frontend/lib/search.tsx
+++ frontend/lib/search.tsx
@@ -6,6 +6,14 @@ export interface SearchResults {
   shown: number
 }
 
+/**
+ * Searches every string (rule, subrule content and chapter, subchapter titles) with the supplied searchTerm.
+ * Minimum searchTerm length is 3.
+ *
+ * @param chapters
+ * @param searchTerm
+ * @returns SearchResults if anything was found and null if not
+ */
 export function search(chapters: types.Chapter[], searchTerm: string): SearchResults | null {
   if (searchTerm.length < 3) {
     return null
@@ -66,6 +74,29 @@ export function search(chapters: types.Chapter[], searchTerm: string): SearchRes
   return { data: results.slice(0, shown), total, shown }
 }
 
+/**
+ * Tells if the term can be found within the supplied text. Case insensitive.
+ *
+ * @param text
+ * @param term
+ * @returns boolean true or false
+ */
+function compare(text: string, term: string): boolean {
+  return text.toLowerCase().includes(term.toLowerCase())
+}
+
+/**
+ * Searches for an exact id match within rules. It's pretty complicated as the ID has multiple parts that can match.
+ * The regex used to match the ID: `/^(\d{3})(\.\d{1,3})?([a-z])?$/i`. Example: 101.2a
+ *   - first digit '1' is the Chapter ID
+ *   - first number '101' before the dot is the Subchapter ID
+ *   - the number '2' after the dot is the Rule ID
+ *   - the last letter is the Subrule ID
+ *
+ * @param searchTerm  The ID to search for.
+ * @param chapters    The chapters to search within.
+ * @returns SearchResult if found and null if not.
+ */
 function findExactId(searchTerm: string, chapters: types.Chapter[]): types.SearchResult | null {
   const ruleIdRegex = /^(\d{3})(\.\d{1,3})?([a-z])?$/i
   const matches = searchTerm.match(ruleIdRegex)
@@ -107,8 +138,4 @@ function findExactId(searchTerm: string, chapters: types.Chapter[]): types.Searc
   }
 
   return null
-}
-
-function compare(text: string, term: string): boolean {
-  return text.toLowerCase().includes(term.toLowerCase())
 }
